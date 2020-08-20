97e412b6234d91a07139ec67715ff05cd464dbcb
Rodrigo Dellagnolo Correia <rodrigo.dellagnolo@gmail.com>
2020-08-19 22:18:48 -0300

    Primeiro commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Esta e uma saudacao: ");
+		System.out.println("Hello, Word!");
+		System.out.printf("Idade: %d\n", 20);
+	}
+}
